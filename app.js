c75f3ba3aff86549478f3889cbfe3f6ffdf1160d
Heli Türi <heli.turi@voco.ee>
2022-12-12 14:49:12 +0200

    Create delete task functionality

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', addTask)
 
+const ul = document.querySelector('ul')
+ul.addEventListener('click', deleteTask)
+
+function deleteTask(event){
+    if(event.target.textContent === 'X'){
+        if(confirm('Are you sure to delete this task?')){
+            event.target.parentElement.remove()
+        }
+    }
+}
 function addTask(event) {
     // console.log(event.type)
     const taskText = document.querySelector('#task').value
@@ -18,7 +28,7 @@ function addTask(event) {
 
     li.appendChild(a)
 
-    const ul = document.querySelector('ul')
+
     ul.appendChild(li)
 
     document.querySelector('#task').value = ''
