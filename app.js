807caefde574349c41ab602088d80a18464297c1
Heli Türi <heli.turi@voco.ee>
2022-12-13 23:29:11 +0200

    Create  reload page get all tasks from localStorage

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', addTask)
-
 const ul = document.querySelector('ul')
 ul.addEventListener('click', deleteTask)
-
 const deleteTasks = document.querySelector('#clear-all-tasks')
 deleteTasks.addEventListener('click', deleteAllTasks)
 
+document.addEventListener('DOMContentLoaded', getTasks)
+
+function getTasks(event){
+    // add task to localStorage
+    let tasks // array for user inputs
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    // loop array for each element value
+    tasks.forEach(function (task){
+        let li = document.createElement('li')
+        li.className = 'collection-item'
+        let liText = document.createTextNode(task)
+        li.appendChild(liText)
+
+        let a = document.createElement('a')
+        a.className = 'teal-text lighten-2 secondary-content'
+        let aText = document.createTextNode('X')
+        a.appendChild(aText)
+        a.setAttribute('href', '#')
+
+        li.appendChild(a)
+
+        ul.appendChild(li)
+    })
+}
+
 function deleteAllTasks(event){
-    //while(ul.firstElementChild){
-    //    ul.removeChild(ul.firstElementChild)
+    // while(ul.firstElementChild){
+    //     ul.removeChild(ul.firstElementChild)
     // }
     ul.innerHTML = ''
     event.preventDefault()
 }
-
 function deleteTask(event){
     if(event.target.textContent === 'X'){
         if(confirm('Are you sure to delete this task?')){
@@ -24,29 +50,23 @@ function deleteTask(event){
 }
 function addTask(event) {
     // user input
-    // console.log(event.type)
     const taskText = document.querySelector('#task').value
-    // console.log(taskText)
     // add DOM element - begin
     let li = document.createElement('li')
     li.className = 'collection-item'
     let liText = document.createTextNode(taskText)
     li.appendChild(liText)
-
     let a = document.createElement('a')
-    a.className= 'teal-text lighten-2 secondary-content'
+    a.className = 'teal-text lighten-2 secondary-content'
     let aText = document.createTextNode('X')
     a.appendChild(aText)
     a.setAttribute('href', '#')
-
     li.appendChild(a)
-
     ul.appendChild(li)
     // add DOM element - end
-
     // add task to localStorage
-    Let  tasks // arau for user imputs
-    if(logalStorage.getitem('tasks') === null){
+    let tasks // array for user inputs
+    if(localStorage.getItem('tasks') === null){
         tasks = []
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'))
